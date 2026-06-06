8cfb43bed5fe1d10e0318f4ddf6a07721b04b505
mohamedamr122 <mohamedamrss123@gmail.com>
2026-06-06 03:51:18 +0300

    fix titles

--- src/components/ShopPage/HeroShopSection.tsx
+++ src/components/ShopPage/HeroShopSection.tsx
@@ -11,11 +11,11 @@ const HeroShopSection = () => {
         className="flex flex-col gap-2"
       >
         <h1 className="inline-block font-headline bg-linear-to-r from-neutral-50 to-primary bg-clip-text text-transparent text-5xl">
-          HARDWARE MATRIX
+          PREMIUM APPAREL
         </h1>
         <p className="text-text max-w-3/4">
-          Precision engineered components for high-frequency computing and elite
-          gaming performance.
+          Expertly designed garments made from high-quality fabrics for a
+          timeless and elegant look.
         </p>
       </motion.div>
       <motion.div
